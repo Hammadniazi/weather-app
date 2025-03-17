f7a4136b2aeb4ebbfdf433c820461848b5322bcd
Hammadniazi <hammadniazi991@gmail.com>
2025-03-17 11:23:51 +0100

    refector code

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-function getWeather() {
+const button = document.querySelector("#getWeather-button");
+button.addEventListener("click", getWeather);
+
+async function getWeather() {
   const location = document.getElementById("location").value;
   const apiKey = "190db5b3e7fa473789e105609251603";
-  const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=yes`;
+  const Api = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=yes`;
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      const weatherDiv = document.getElementById("weather-result");
-      if (data.error) {
-        weatherDiv.innerHTML = `<p style="color: red;">${data.error.message}</p>`;
-      } else {
-        weatherDiv.innerHTML = `
-                    <h2>${data.location.name}, ${data.location.country}</h2>
-                    <p>Temperature: ${data.current.temp_c}°C</p>
-                    <p>Condition: ${data.current.condition.text}</p>
-                    <img src="${data.current.condition.icon}" alt="Weather icon">
-                `;
-      }
-    })
-    .catch((error) => console.log("Error:", error));
+  try {
+    const response = await fetch(Api);
+    const data = await response.json();
+
+    const weatherDiv = document.getElementById("weather-result");
+    if (data.error) {
+      weatherDiv.innerHTML = `<p style="color: red;">${data.error.message}</p>`;
+    } else {
+      weatherDiv.innerHTML = `
+                      <h2>${data.location.name}, ${data.location.country}</h2>
+                      <p>Temperature: ${data.current.temp_c}°C</p>
+                      <p>Local Time: ${data.location.localtime}</p>
+                      <p>Condition: ${data.current.condition.text}</p>
+                      <img src="${data.current.condition.icon}" alt="Weather icon">
+                  `;
+    }
+  } catch (error) {
+    console.error("Something is wrong!", error?.message);
+  }
 }
